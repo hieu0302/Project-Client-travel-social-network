edf365e7b5e531cbcf33631c2d59250f6cd27e4b
hieu0302 <ferdhjeu1@gmail.com>
2024-01-20 19:33:13 +0700

    fix socket io

--- src/components/Socket/Soket.js
+++ src/components/Socket/Soket.js
@@ -2,6 +2,6 @@ import io from "socket.io-client";
 
 // const socket = io("http://localhost:4000");
 
-const socket = io("https://trip-social-socket.onrender.com");
+const socket = io("https://trip-social-socket.onrender.com:4000");
 
 export default socket;
